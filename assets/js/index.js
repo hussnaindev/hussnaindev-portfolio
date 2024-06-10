fedc09e334ce618875f92d1f43562a807078cf99
Hussnain Raza <hussnain444all@gmail.com>
2024-06-10 21:38:59 +0500

    updated info

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
 function openEmailInCurrentTab() {
-	const mailtoUrl = `mailto:justehmadsaeed@gmail.com`;
+	const mailtoUrl = `mailto:hussnain444all@gmail.com`;
 	window.location.assign(mailtoUrl);
 }
